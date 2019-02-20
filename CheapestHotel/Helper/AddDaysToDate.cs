bd545b4804936ecfe134484c1b0acf4041670f7c
Muskan <muskan.sethia14@gmail.com>
2019-02-20 16:15:54 +0530

    Modified names

---
+++ CheapestHotel/Helper/AddDaysToDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapestHotel.Helper
+{
+    public static class AddDaysToDate
+    {
+        public static DateTime AddDaysToCheckInDate(DateTime checkInDate, int valueToAdd)
+        {
+            return checkInDate.AddDays(valueToAdd);
+        }
+    }
+}
